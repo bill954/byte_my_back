86362a9d42b5977a60b999d8c20492c5976daaad
bill954 <raafaa9573@gmail.com>
2023-03-09 21:48:14 -0300

    Added a  model for Cards.

--- django_base/coins/admin.py
+++ django_base/coins/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from coins.models import Card
 
-# Register your models here.
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('card_name',)
--- django_base/coins/models.py
+++ django_base/coins/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+min_value = 4300000000000000
+max_value = 4399999999999999
+
+class Card(models.Model):
+    
+    CHOICES = {
+        ('purple', 'purple'),
+        ('blue', 'blue'),
+        ('green', 'green'),
+        ('orange', 'orange')
+    }
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cards')
+    card_name = models.CharField(max_length=40)
+    card_holder = models.CharField(max_length=60)
+    card_number = models.IntegerField(validators=[MinValueValidator(min_value), MaxValueValidator(max_value)])
+    bank_name = models.CharField(max_length=80)
+    valid_date = models.DateField()
+    color = models.CharField(max_length=10, choices=CHOICES, default='purple')
+    
+    def __str__(self):
+        return self.card_name
